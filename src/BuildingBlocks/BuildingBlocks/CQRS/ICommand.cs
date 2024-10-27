8d7ebe9372cf22279e3ad8b8e5bd7586a8dc75bc
Gururaj KL <gururaj.kl@sasken.com>
2024-10-27 09:07:00 +0530

    Implement cqrs for mediatR

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommand : ICommand<Unit> // Unit is a void type for MedatR.
+{
+
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
