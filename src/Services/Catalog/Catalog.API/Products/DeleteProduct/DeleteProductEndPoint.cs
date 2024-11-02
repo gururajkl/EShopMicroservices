ea2fcf42ad7fa81a7faccfb17ba957bf74d51ad6
Gururaj KL <gururaj.kl@sasken.com>
2024-11-02 13:57:17 +0530

    Implement delete product in catalog microservices

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndPoint.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductRequest(Guid Id);
+public record DeleteProductResponse(bool IsSuccess);
+
+public class DeleteProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(id));
+            var response = result.Adapt<DeleteProductResponse>();
+            return Results.Ok(response);
+        }).WithName("DeleteProduct") // Response descriptions and details.
+          .WithDescription("Delete Product")
+          .WithSummary("Delete Product")
+          .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+/// <summary>
+/// Command helps to delete the product.
+/// </summary>
+/// <param name="Id">ID of the product.</param>
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+/// <summary>
+/// Result pattern for the delete command.
+/// </summary>
+/// <param name="IsSuccess">Indicates whether the product update operation succeeded.</param>
+public record DeleteProductResult(bool IsSuccess);
+
+// Handler class for DeleteProductCommand.
+internal class DeleteProductCommandHandler(IDocumentSession session) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+        return new DeleteProductResult(true);
+    }
+}
