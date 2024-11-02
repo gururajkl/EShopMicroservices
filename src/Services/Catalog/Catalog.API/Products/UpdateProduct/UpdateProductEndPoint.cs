98e22d9d07cb56570cdb90c2ffce9d430a13a831
Gururaj KL <gururaj.kl@sasken.com>
2024-11-02 13:18:58 +0530

    Implement update product in catalog microservice

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndPoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<UpdateProductResponse>;
+public record UpdateProductResponse(bool IsSuccess);
+
+public class UpdateProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<UpdateProductResponse>();
+            return Results.Ok(response);
+        }).WithName("UpdateProduct") // Response descriptions and details.
+          .WithDescription("Update Product")
+          .WithSummary("Update Product")
+          .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<UpdateProductResult>;
+public record UpdateProductResult(bool IsSuccess);
+
+internal class UpdateProductCommandHandler(IDocumentSession session) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        var productFromDb = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (productFromDb is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        productFromDb.Name = command.Name;
+        productFromDb.Category = command.Category;
+        productFromDb.Description = command.Description;
+        productFromDb.ImageFile = command.ImageFile;
+        productFromDb.Price = command.Price;
+
+        // Update in the database.
+        session.Update(productFromDb);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new UpdateProductResult(true);
+    }
+}
