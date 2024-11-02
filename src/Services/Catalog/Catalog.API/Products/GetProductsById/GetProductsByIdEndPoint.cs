0dc309bee4636ef8a894f77bc9c0cc507d05ce11
Gururaj KL <gururaj.kl@sasken.com>
2024-11-02 09:49:59 +0530

    Implement get product by id endpoint and handler for catalog microservice

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsById/GetProductsByIdEndPoint.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.GetProductsById;
+
+public record GetProductByIdRequest(Guid Id);
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductsByIdEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+            var response = result.Adapt<GetProductByIdResponse>();
+            return Results.Ok(response);
+        }).WithName("GetProductById").WithDescription("Get Product By Id").WithSummary("Get Product By Id")
+          .Produces<GetProductByIdResponse>(StatusCodes.Status201Created).ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsById/GetProductsByIdHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.API.Products.GetProductsById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+internal class GetProductsByIdQueryHandler(IDocumentSession session, ILogger<GetProductsByIdQueryHandler> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"GetProductsQueryHandler.Handle called {query}");
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        return product is null ? throw new ProductNotFoundException() : new GetProductByIdResult(product);
+    }
+}
