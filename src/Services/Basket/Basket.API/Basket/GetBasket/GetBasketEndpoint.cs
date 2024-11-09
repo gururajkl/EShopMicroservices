3b82dfa89d5b8808788f9166646778a00088df8e
gururajkl <gururajragavendra7@gmail.com>
2024-11-09 18:43:02 +0530

    Create skeleton handler and endpoints of get basket

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,5 +1,20 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketEndpoint
+public record GetBasketResponse(ShoppingCart ShoppingCart);
+
+public class GetBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetBasketQuery(userName));
+            var response = result.Adapt<GetBasketResponse>();
+            return Results.Ok(response);
+        }).WithName("GetBasket")
+          .WithDescription("Get Basket")
+          .WithSummary("Get Basket")
+          .Produces<GetBasketResponse>(StatusCodes.Status201Created)
+          .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
 }
--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,5 +1,13 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketHandler
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart ShoppingCart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
+    public async Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+    {
+        // TODO: Use MediatR and PostgreSQL.
+        return new(new ("Dummy Item"));
+    }
 }
