e4c17a0aa38820c0dca2a2ee12b4f8950c446bda
gururajkl <gururajragavendra7@gmail.com>
2024-11-24 12:22:12 +0530

    Seed and Auto-Migrate SQLite Database using Entity Framework Core

--- src/Services/Discount/Discount.Grpc/Data/DiscountDataContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountDataContext.cs
@@ -6,4 +6,12 @@ namespace Discount.Grpc.Data;
 public class DiscountDataContext(DbContextOptions<DiscountDataContext> dbContextOptions) : DbContext(dbContextOptions)
 {
     public DbSet<Coupon> Coupones { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+            new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+            );
+    }
 }
