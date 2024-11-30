575d50a310c8be3043c83048d3ace165725e63de
gururajkl <gururajragavendra7@gmail.com>
2024-11-30 18:10:17 +0530

    Develop DDD Abstractions (Entity, Aggregate, DomainEvent) in Ordering.Domain

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+/// <summary>
+/// Defines the abstraction for a domain entity with a generic identifier and tracking properties.
+/// </summary>
+/// <typeparam name="T">The type of the identifier for the entity.</typeparam>
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+
+/// <summary>
+/// Represents the base interface for domain entities with audit metadata.
+/// </summary>
+public interface IEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
