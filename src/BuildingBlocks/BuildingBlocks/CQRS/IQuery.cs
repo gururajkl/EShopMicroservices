6f794146fb4a60ed9e4e31d00e0b1b24e5d34cb3
gururajkl <gururajragavendra7@gmail.com>
2025-06-01 10:42:42 +0530

    Refactor CQRS interfaces and project references
    Updated ICommand and IQuery interfaces to include namespace
    declarations and enable covariance with the 'out' keyword.
    Removed MediatR package reference from Catalog.API.csproj
    and added a project reference to BuildingBlocks.csproj.
    Introduced global using for MediatR and added ICommandHandler
    and IQueryHandler interfaces for better command and query
    handler definitions.

--- src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -1,6 +1,4 @@
-﻿using MediatR;
-
-namespace BuildingBlocks.CQRS;
+﻿namespace BuildingBlocks.CQRS;
 
 // The 'out' keyword enables covariance, indicating that TResponse is used only as a return / output type, not as an input.
 public interface IQuery<out TResponse> : IRequest<TResponse> where TResponse : notnull 
