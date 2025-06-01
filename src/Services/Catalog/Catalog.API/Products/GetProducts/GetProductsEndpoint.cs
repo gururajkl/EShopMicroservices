b533ce1c5c447a8c209c5d0bf3417a7c206356ec
gururajkl <gururajragavendra7@gmail.com>
2025-06-01 18:42:05 +0530

    Add CQRS-based product retrieval feature

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            GetProductsResult getProductsResult = await sender.Send(new GetProductsQuery());
+            GetProductsResponse getProductsResponse = getProductsResult.Adapt<GetProductsResponse>();
+            return Results.Ok(getProductsResponse);
+        })
+        .WithName("GetProducts")
+        .WithDescription("Get products")
+        .WithSummary("Get products with CQRS pattern")
+        .Produces<GetProductsResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
