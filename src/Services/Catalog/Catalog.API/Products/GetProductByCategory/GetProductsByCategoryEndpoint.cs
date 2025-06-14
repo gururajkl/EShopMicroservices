f7c3b7ca9f3ad98707679ef1660324d8eb2cfcb8
gururajkl <gururajragavendra7@gmail.com>
2025-06-14 10:51:36 +0530

    Refactor product category fetching classes and records

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductsByCategoryEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductsByCategoryEndpoint.cs
@@ -2,13 +2,13 @@
 
 public record GetProductsByCategoryResponse(IEnumerable<Product> Products);
 
-public class GetProductByCategoryEndpoint : ICarterModule
+public class GetProductsByCategoryEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
         {
-            var result = await sender.Send(new GetProductByCategoryQuery(category));
+            var result = await sender.Send(new GetProductsByCategoryQuery(category));
             var response = result.Adapt<GetProductsByCategoryResponse>();
             return response;
         })
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+public class GetProductsByCategoryQueryHandler(IDocumentSession session,
+    ILogger<GetProductsByCategoryQueryHandler> logger) : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+{
+    public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"GetProductByCategoryQueryHandler.Handle called for the query: {query}");
+
+        var productsByCategory = await session.Query<Product>().Where(p => p.Category.Contains(query.Category)).ToListAsync(cancellationToken);
+
+        return new GetProductsByCategoryResult(productsByCategory);
+    }
+}
