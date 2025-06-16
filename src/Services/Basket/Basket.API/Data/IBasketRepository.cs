4b56c6cc438a3ba3e3bf38bd664ec9e12be5b4b1
gururajkl <gururajragavendra7@gmail.com>
2025-06-16 18:29:59 +0530

    Add Marten support and implement basket repository
    - Updated `Basket.API.csproj` to include `Marten` and Azure container tools.
    - Added global using directives for new namespaces in `GlobalUsings.cs`.
    - Introduced `BasketRepository` class for CRUD operations on `ShoppingCart`.
    - Created `IBasketRepository` interface to define basket operation contracts.
    - Added `BasketNotFoundException` for handling missing baskets.

---
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Data;
+
+/// <summary>
+/// Contract to do Basket CRUD operation.
+/// </summary>
+public interface IBasketRepository
+{
+    Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default);
+    Task<ShoppingCart> StoreBasket(ShoppingCart shoppingCart, CancellationToken cancellationToken = default);
+    Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default);
+}
--- src/Services/Basket/Basket.API/GlobalUsings.cs
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -1,10 +1,11 @@
-﻿global using BuildingBlocks.Behaviors;
+﻿global using Basket.API.Basket.GetBasket;
+global using Basket.API.Exceptions;
+global using Basket.API.Models;
+global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Exceptions;
-global using BuildingBlocks.Exceptions.Handler;
+global using Carter;
 global using FluentValidation;
 global using Mapster;
+global using Marten;
 global using MediatR;
-global using Basket.API.Models;
-global using Carter;
-global using Basket.API.Basket.GetBasket;
