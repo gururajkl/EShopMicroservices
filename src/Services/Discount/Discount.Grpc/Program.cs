8960061ec1b8cf96d86a503196682633790bfd56
gururajkl <gururajragavendra7@gmail.com>
2025-06-18 21:41:14 +0530

    Add Entity Framework Core support with SQLite
    Updated project to include Entity Framework Core packages for SQLite.
    Registered a new `DiscountContext` in `Program.cs` with a connection string in `appsettings.json`.
    Removed the `Protos\discount.proto` entry from the project file.
    Created `DiscountContext` class with a `DbSet<Coupon>` for managing coupons.

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,9 +1,17 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+
+// Register DbContext using SQLLite connection string from configuration.
+builder.Services.AddDbContext<DiscountContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database")!);
+});
 
 var app = builder.Build();
 
