adaae7defdd0f0def5e9f5a34063f166889fe42d
gururajkl <gururajragavendra7@gmail.com>
2025-06-18 22:00:26 +0530

    Add DiscountContext and initial migration setup
    This commit introduces a new `DiscountContext` class for managing coupons using Entity Framework Core. It includes a `DbSet<Coupon>` and seeds the database with initial data. Middleware is added in `Program.cs` to apply migrations at startup, and the connection string for the SQLite database is configured in `appsettings.json`. Migration files are created to define the `Coupons` table schema, and the model snapshot is updated to reflect the current state. Overall, these changes enhance the application's database management capabilities.

---
+++ src/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public static class Extensions
+{
+    // Apply pending migrations at startup.
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        context.Database.MigrateAsync();
+        return app;
+    }
+}
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -15,9 +15,9 @@ builder.Services.AddDbContext<DiscountContext>(options =>
 
 var app = builder.Build();
 
+app.UseMigration(); // Apply pending migrations at startup.
+
 // Configure the HTTP request pipeline.
 app.MapGrpcService<DiscountService>();
-
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
