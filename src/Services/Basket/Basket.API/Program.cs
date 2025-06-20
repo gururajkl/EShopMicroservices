fb663c4c594f033877447b433fbb8b6d60220a8a
gururajkl <gururajragavendra7@gmail.com>
2025-06-20 21:36:38 +0530

    Add gRPC client handler and update Docker configuration
    - Updated `Program.cs` to include a custom HTTP message handler for the gRPC client, allowing acceptance of any server certificate.
    - Added Redis connection string in `docker-compose.override.yml` and updated `depends_on` to include `discount.grpc`.
    - Modified port mappings for external access to the gRPC service on port 8081.
    - Noted binary file differences in `discountdb-shm` and `discountdb-wal`.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -44,6 +44,14 @@ builder.Services.AddStackExchangeRedisCache(option =>
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
 {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+}).ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+
+    return handler;
 });
 
 #region Cross cutting services.
