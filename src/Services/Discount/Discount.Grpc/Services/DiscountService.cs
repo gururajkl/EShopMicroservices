2c36511e757ce0b28050ab1effafd2341b4b16bb
gururajkl <gururajragavendra7@gmail.com>
2025-06-20 19:32:17 +0530

    Add Mapster for object mapping and update DiscountService
    - Updated `Discount.Grpc.csproj` to include `Mapster` v7.4.0.
    - Added necessary using directives in `DiscountService.cs`.
    - Modified `DiscountService` constructor for dependency injection of `DiscountContext` and `ILogger`.
    - Refactored `GetDiscount` method to asynchronously retrieve coupons from the database, with error handling for missing coupons.
    - Updated return type of `GetDiscount` to use `Mapster` for mapping to `CouponModel`.
    - Modified binary files `discountdb-shm` and `discountdb-wal` reflecting database changes.

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,12 +1,29 @@
-﻿using Grpc.Core;
+﻿using Discount.Grpc.Data;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services;
 
-public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger)
+    : DiscountProtoService.DiscountProtoServiceBase // Base class is generated from the .proto file
 {
-    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        return base.GetDiscount(request, context);
+        var coupon = await dbContext.Coupons.FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+
+        if (coupon is null)
+        {
+            logger.LogError($"Coupon with {request.ProductName} not found");
+            return new CouponModel()
+            {
+                ProductName = "No product",
+                Amount = 0,
+                Description = "No description",
+            };
+        }
+
+        return coupon.Adapt<CouponModel>();
     }
 
     public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
