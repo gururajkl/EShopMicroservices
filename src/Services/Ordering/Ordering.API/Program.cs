57c4c1b6224e81a67897abe974c0b2590e0ea082
gururajkl <gururajragavendra7@gmail.com>
2025-06-21 14:21:12 +0530

    Refactor dependency injection structure
    Introduce a modular approach for dependency injection across
    `Ordering.API`, `Ordering.Application`, and `Ordering.Infrastructure` namespaces.
    Set up service registration in `Program.cs` with a consolidated method call.
    Define service extension methods in each namespace, preparing for future enhancements
    while promoting separation of concerns and improving dependency management.

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,17 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service to the container.
+// Add service injections using extension method.
+
+// Infrastructure - Ef core.
+// Appliacation - MediatR.
+// API - Carter, Health checks...
+// These are all extension methods.
+builder.Services.AddApplicationService().AddInfrastructureService(builder.Configuration).AddApiServices();
 
 var app = builder.Build();
 
