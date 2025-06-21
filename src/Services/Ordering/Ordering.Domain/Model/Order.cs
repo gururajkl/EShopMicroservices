a64f578cfb01cac35794ec9cd692c692c380861c
gururajkl <gururajragavendra7@gmail.com>
2025-06-21 20:28:52 +0530

    Refactor Order class and update related entities
    - Renamed `OrderStatus` to `Status` in the `Order` class.
    - Added methods for creating, updating, adding, and removing order items.
    - Changed `OrderItem` constructor access from public to internal.
    - Updated `Product` class to change `Price` type from `string` to `decimal` and added a static `Create` method for validation.

--- src/Services/Ordering/Ordering.Domain/Model/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Model/Order.cs
@@ -11,7 +11,7 @@ public class Order : Aggregate<OrderId>
     public Address ShippingAddress { get; private set; } = default!;
     public Address BillingAddress { get; private set; } = default!;
     public Payment Payment { get; private set; } = default!;
-    public OrderStatus OrderStatus { get; private set; } = OrderStatus.Pending;
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
     public decimal TotalPrice
     {
         get => OrderItems.Sum(o => o.Price * o.Quantity);
@@ -19,4 +19,53 @@ public class Order : Aggregate<OrderId>
     }
 
     // Business logic.
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress,
+        Address billingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending,
+        };
+
+        order.AddDomainEvents(new OrderCreatedEvent(order));
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress,
+        Address billingAddress, Payment payment)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+        Status = OrderStatus.Pending;
+
+        AddDomainEvents(new OrderUpdatedEvent(this));
+    }
+
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity, nameof(quantity));
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price, nameof(price));
+
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+
+        _orderItems.Add(orderItem);
+    }
+
+    public void Remove(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(o => o.ProductId == productId);
+
+        if (orderItem is not null)
+        {
+            _orderItems.Remove(orderItem);
+        }
+    }
 }
