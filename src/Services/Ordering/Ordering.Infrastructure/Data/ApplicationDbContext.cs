2cf0e46ee56a3cf0653b22b669fc62ec806ac71b
gururajkl <gururajragavendra7@gmail.com>
2025-06-22 11:16:15 +0530

    Add ApplicationDbContext for Entity Framework Core
    Introduces a new `ApplicationDbContext` class inheriting from `DbContext`. This class manages the database context and includes `DbSet` properties for `Customer`, `Product`, `Order`, and `OrderItem` entities. The `OnModelCreating` method is overridden to apply configurations from the current assembly, enhancing modularity in entity configuration.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Model;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // This line scans the assembly and uses the class which is implementing IEntityConfiguration.
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
