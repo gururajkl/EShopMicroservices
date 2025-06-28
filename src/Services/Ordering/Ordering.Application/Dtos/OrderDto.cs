b924b13fc025788844c579b6ec31a9a5b77ff8d6
gururajkl <gururajragavendra7@gmail.com>
2025-06-28 18:59:35 +0530

    Add DTOs for Address, Order, OrderItem, and Payment
    Updated GlobalUsings to include Ordering.Domain.Enums.
    Removed Dtos folder reference from the project file.
    Introduced new record types: AddressDto, OrderDto, OrderItemDto, and PaymentDto to encapsulate relevant properties for address, order details, order items, and payment information.

---
+++ src/Services/Ordering/Ordering.Application/Dtos/OrderDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Ordering.Application.Dtos;
+
+public record OrderDto(Guid Id, Guid CustomerId, string OrderName, AddressDto ShippingAddress, AddressDto BillingAddress,
+    PaymentDto Payment, OrderStatus OrderStatus, List<OrderItemDto> OrderItem);
--- src/Services/Ordering/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsings.cs
@@ -1,1 +1,2 @@
-﻿
+﻿global using Ordering.Application.Dtos;
+global using Ordering.Domain.Enums;
