80a3e6121ea43a59839251b9b564a19655d0dd0c
gururajkl <gururajragavendra7@gmail.com>
2025-06-28 19:19:58 +0530

    Add CreateOrder command, validator, and handler
    This commit introduces a new command for creating orders in the Ordering application.
    It includes the `CreateOrderResult` record to represent command results,
    a `CreateOrderCommandValidator` for validating command properties,
    and a `CreateOrderHandler` that implements the command handling interface.
    Additionally, the project file has been updated to remove the folder reference
    for "Orders\Commands\", indicating a potential restructuring of the project.

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Order.CustomerId).NotEmpty().WithMessage("Customer Id is required");
+        RuleFor(x => x.Order.OrderItem).NotEmpty().WithMessage("Order Item is required");
+    }
+}
+
+public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
