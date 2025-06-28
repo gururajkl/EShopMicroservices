7f73144dbaa8c1ce39695cf7961f9e5a9db800a5
gururajkl <gururajragavendra7@gmail.com>
2025-06-28 11:45:26 +0530

    Refactor IEntity interface LastModifiedBy property
    Changed LastModifiedBy from DateTime to nullable string to allow for more flexible representation of the last modifier's identity.

--- src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -5,7 +5,7 @@ public interface IEntity
     public DateTime? CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
     public DateTime? LastModifiedAt { get; set; }
-    public DateTime LastModifiedBy { get; set; }
+    public string? LastModifiedBy { get; set; }
 }
 
 public interface IEntity<T> : IEntity
