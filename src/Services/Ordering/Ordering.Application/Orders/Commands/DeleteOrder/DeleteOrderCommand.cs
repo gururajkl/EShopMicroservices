70816e22e0e8dd0d1f3a91dae82b4fdd98d7c572
gururajkl <gururajragavendra7@gmail.com>
2025-06-29 10:03:13 +0530

    Add delete order feature with validation
    Introduces `DeleteOrderCommand` and `DeleteOrderResult` for handling order deletions. Adds `DeleteOrderCommandValidator` to ensure `OrderId` is not empty. Implements `DeleteOrderHandler` to manage the deletion process, including order retrieval and exception handling for non-existent orders.

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public record DeletOrderCommand(Guid OrderId) : ICommand<DeleteOrderResult>;
+
+public record DeleteOrderResult(bool IsSuccess);
+
+public class DeleteOrderCommandValidator : AbstractValidator<DeletOrderCommand>
+{
+    public DeleteOrderCommandValidator()
+    {
+        RuleFor(command => command.OrderId).NotEmpty().WithMessage("Order Id is required.");
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderHandler(IApplicationDbContext dbContext) : ICommandHandler<DeletOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeletOrderCommand command, CancellationToken cancellationToken)
+    {
+        // Get the order from DB.
+        OrderId orderId = OrderId.Of(command.OrderId);
+        var order = await dbContext.Orders.FindAsync([orderId], cancellationToken);
+
+        if (order is null)
+        {
+            throw new OrderNotFoundException(command.OrderId);
+        }
+
+        // Delete the order.
+        dbContext.Orders.Remove(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteOrderResult(true);
+    }
+}
