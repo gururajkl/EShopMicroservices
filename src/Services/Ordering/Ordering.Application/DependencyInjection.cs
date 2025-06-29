8020c3e9737f20945f68b55b5b7ebe10fce32441
gururajkl <gururajragavendra7@gmail.com>
2025-06-29 18:32:29 +0530

    Add validation and logging behaviors to DI
    Added using directives for BuildingBlocks.Behaviors in
    DependencyInjection.cs. Registered ValidationBehavior<,>
    and LoggingBehavior<,> in the AddApplicationService method
    to enhance MediatR configuration for better validation
    and logging handling.

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Ordering.Application;
@@ -15,6 +16,8 @@ public static class DependencyInjection
         services.AddMediatR(cfg =>
         {
             cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
         return services;
