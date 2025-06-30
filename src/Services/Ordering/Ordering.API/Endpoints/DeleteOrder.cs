e50f7d09840f3499f91d433f672d51b80e786798
gururajkl <gururajragavendra7@gmail.com>
2025-06-30 22:59:36 +0530

    Add delete order feature in DeleteOrder.cs
    Introduces a new `DeleteOrder` class implementing `ICarterModule` to handle HTTP DELETE requests for deleting orders by ID. Adds a `DeleteOrderResponse` record to encapsulate the result of the operation. Configures the route with appropriate status codes for success and error responses.

---
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -0,0 +1,24 @@
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints;
+
+public record DeleteOrderResponse(bool IsSuccess);
+
+public class DeleteOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeletOrderCommand(id));
+            var response = result.Adapt<DeleteOrderResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("DeleteOrder")
+        .WithDescription("Delete Order")
+        .WithSummary("Delete order with CQRS pattern")
+        .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound);
+    }
+}
