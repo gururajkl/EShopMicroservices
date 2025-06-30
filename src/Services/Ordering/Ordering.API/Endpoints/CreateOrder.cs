244a15ef9a5a03cafd0d43f073eb6baaf7169a77
gururajkl <gururajragavendra7@gmail.com>
2025-06-30 22:50:21 +0530

    Add CreateOrder endpoint with request/response models
    This commit introduces a new endpoint for creating orders in the `CreateOrder.cs` file. It defines `CreateOrderRequest` and `CreateOrderResponse` records for encapsulating order data and returning the order ID, respectively. The `CreateOrder` class implements the `ICarterModule` interface and sets up a POST route at "/orders" to handle order creation. The `GlobalUsings.cs` file is also updated to include necessary namespaces for the new functionality, such as `Carter`, `Mapster`, `MediatR`, and application-specific DTOs and commands.

---
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -0,0 +1,24 @@
+﻿namespace Ordering.API.Endpoints;
+
+public record CreateOrderRequest(OrderDto Order);
+public record CreateOrderResponse(Guid Id);
+
+public class CreateOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) =>
+        {
+            var createOrderCommand = request.Adapt<CreateOrderCommand>();
+            CreateOrderResult createOrderResult = await sender.Send(createOrderCommand);
+            var response = createOrderResult.Adapt<CreateOrderResponse>();
+            return Results.Created($"/orders/{response.Id}", response);
+        })
+        .WithName("CreateOrder")
+        .WithDescription("Create Order")
+        .WithSummary("Create with CQRS pattern")
+        .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound);
+    }
+}
