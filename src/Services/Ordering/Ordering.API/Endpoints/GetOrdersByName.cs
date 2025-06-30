f0817b40f26718f8bb005ea63d12a7c845762839
gururajkl <gururajragavendra7@gmail.com>
2025-06-30 23:05:24 +0530

    Add endpoint to retrieve orders by name
    Introduces a new endpoint in `GetOrdersByName.cs` for fetching orders based on the order name. This includes a new response record `GetOrdersByNameResponse` containing a list of `OrderDto` objects. The `GetOrdersByName` class implements the `ICarterModule` interface and defines a GET route at `/orders/{orderName}` using the CQRS pattern. The endpoint is equipped with metadata and specifies expected HTTP status codes for various responses.

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -0,0 +1,24 @@
+﻿using Ordering.Application.Orders.Queries.GetOrdersByName;
+
+namespace Ordering.API.Endpoints;
+
+public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+
+public class GetOrdersByName : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/{orderName}", async (string orderName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByNameQuery(orderName));
+            var response = result.Adapt<GetOrdersByNameResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetOrdersByName")
+        .WithDescription("GetOrdersByName")
+        .WithSummary("GetOrdersByName with CQRS pattern")
+        .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound);
+    }
+}
