95e251c7dd997f41f7f3055ab9c7692b6bf4d54a
gururajkl <gururajragavendra7@gmail.com>
2025-06-30 20:41:43 +0530

    Refactor query handling and update global usings
    - Added global usings for MediatR, EF Core, and Logging.
    - Removed unused global usings to streamline code.
    - Restructured project references in Ordering.Application.csproj.
    - Introduced GetOrdersByNameHandler for processing queries.
    - Added GetOrdersByNameQuery and GetOrdersByNameResult records to support CQRS pattern.

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public class GetOrdersByNameHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        // Include OrderItems in the query to fetch related data.
+        var orders = await dbContext.Orders.Include(o => o.OrderItems).AsNoTracking().Where(o => o.OrderName.Value.Contains(query.Name)).OrderBy(o => o.OrderName)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersByNameResult(ProjectToOrderDto(orders));
+    }
+
+    private List<OrderDto> ProjectToOrderDto(List<Order> orders)
+    {
+        List<OrderDto> orderDtos = [];
+
+        foreach (var order in orders)
+        {
+            var orderDto = new OrderDto(
+                order.Id.Value,
+                order.CustomerId.Value,
+                order.OrderName.Value,
+                new AddressDto(order.ShippingAddress.FirstName, order.ShippingAddress.LastName, order.ShippingAddress.EmailAddress!,
+                    order.ShippingAddress.AddressLine, order.ShippingAddress.Country, order.ShippingAddress.State, order.ShippingAddress.ZipCode),
+                new AddressDto(order.BillingAddress.FirstName, order.BillingAddress.LastName, order.BillingAddress.EmailAddress!,
+                    order.BillingAddress.AddressLine, order.BillingAddress.Country, order.BillingAddress.State, order.BillingAddress.ZipCode),
+                new PaymentDto(order.Payment.CardName!, order.Payment.CardNumber, order.Payment.Expiration, order.Payment.CVV,
+                    order.Payment.PaymentMethod),
+                order.Status,
+                order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList()
+            );
+
+            orderDtos.Add(orderDto);
+        }
+
+        return orderDtos;
+    }
+}
