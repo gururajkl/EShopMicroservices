a394c94751f93a124edbca1f5dc492a771ec3ccf
gururajkl <gururajragavendra7@gmail.com>
2025-06-30 22:39:18 +0530

    Add Carter package to the ordering api

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿namespace Ordering.API;
+﻿using Carter;
+
+namespace Ordering.API;
 
 public static class DependencyInjection
 {
@@ -9,12 +11,15 @@ public static class DependencyInjection
     /// <returns>Service object with all added services.</returns>
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
+        services.AddCarter();
+
         return services;
     }
 
     // After buidling the app.
     public static WebApplication UseApiServices(this WebApplication app)
     {
+        app.MapCarter();
         return app;
     }
 }
