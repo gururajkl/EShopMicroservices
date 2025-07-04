df4395cea8fadb664b69240a33f92bd45bcdcca4
gururajkl <gururajragavendra7@gmail.com>
2025-07-04 19:56:53 +0530

    Refactor messaging setup and add RabbitMQ configuration
    - Removed folder reference for "MassTransit" in the project file.
    - Added "MessageBroker" configuration in `appsettings.json` for RabbitMQ.
    - Introduced `Extensions.cs` with a method to configure MassTransit for RabbitMQ.

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+    {
+        // Implement rabbitmq masstransit configuration here
+        return services;
+    }
+}
