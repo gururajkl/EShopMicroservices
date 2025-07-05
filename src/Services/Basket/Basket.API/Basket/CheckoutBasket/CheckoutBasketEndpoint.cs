32f20b4ce065fcc7814cb3b81076b1f67ffab76f
gururajkl <gururajragavendra7@gmail.com>
2025-07-05 09:40:30 +0530

    Add checkout functionality for basket operations
    Introduce request/response records, command handler, and
    validator for checkout process. Create a new DTO to
    encapsulate user and payment details. Define API route
    in `CheckoutBasketEndpoint` and implement processing
    logic in `CheckoutBasketHandler`.

--- src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketEndpoint.cs
@@ -1,5 +1,25 @@
-﻿namespace Basket.API.Basket.CheckoutBasket;
+﻿using Basket.API.Dtos;
 
-public class CheckoutBasketEndpoint
+namespace Basket.API.Basket.CheckoutBasket;
+
+public record CheckoutBasketRequest(BasketCheckoutDto BasketCheckoutDto);
+public record CheckoutBasketResponse(bool IsSuccess);
+
+public class CheckoutBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket/checkout", async (CheckoutBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CheckoutBasketCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CheckoutBasketResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("CheckoutBasket")
+        .WithDescription("Checkout Basket")
+        .WithSummary("Checkout basket with CQRS pattern")
+        .Produces<CheckoutBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
 }
--- src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -1,5 +1,42 @@
-﻿namespace Basket.API.Basket.CheckoutBasket;
+﻿using Basket.API.Dtos;
+using BuildingBlocks.Messaging.Events;
+using MassTransit;
 
-public class CheckoutBasketHandler
+namespace Basket.API.Basket.CheckoutBasket;
+
+public record CheckoutBasketCommand(BasketCheckoutDto BasketCheckoutDto) : ICommand<CheckoutBasketResult>;
+public record CheckoutBasketResult(bool IsSuccess);
+
+public class CheckoutBasketCommandValidator : AbstractValidator<CheckoutBasketCommand>
 {
+    public CheckoutBasketCommandValidator()
+    {
+        RuleFor(x => x.BasketCheckoutDto).NotNull().WithMessage("Basket checkout dto cannot be null");
+        RuleFor(x => x.BasketCheckoutDto.UserName).NotEmpty().WithMessage("Username is required");
+    }
+}
+
+public class CheckoutBasketCommandHandler(IBasketRepository repository,
+    IPublishEndpoint publishEndpoint) : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+{
+    public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+    {
+        // Get basket.
+        var basket = await repository.GetBasket(command.BasketCheckoutDto.UserName, cancellationToken);
+        if (basket is null)
+        {
+            return new(false);
+        }
+
+        // Publish message to rabbitmq.
+        var eventMessage = command.BasketCheckoutDto.Adapt<BasketCheckoutEvent>();
+        eventMessage.TotalPrice = basket.TotalPrice;
+
+        await publishEndpoint.Publish(eventMessage, cancellationToken);
+
+        // Delete basket after publishing.
+        await repository.DeleteBasket(command.BasketCheckoutDto.UserName, cancellationToken);
+
+        return new(true);
+    }
 }
