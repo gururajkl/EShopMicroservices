b1d87de19059e1dc457f9597acd5b5b9d0c0c041
gururajkl <gururajragavendra7@gmail.com>
2025-07-06 14:23:34 +0530

    Create Basket and Catalog domain model in web application

---
+++ src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping.Web.Models.Basket;
+
+public class ShoppingCartModel
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItemModel> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(i => i.Price * i.Quantity);
+}
+
+
+// Wrapper for the response from the API.
+public record GetBasketResponse(ShoppingCartModel ShoppingCart);
+public record StoreBasketRequest(ShoppingCartModel Cart);
+public record StoreBasketResponse(string UserName);
+public record DeleteBasketResponse(bool IsSuccess);
