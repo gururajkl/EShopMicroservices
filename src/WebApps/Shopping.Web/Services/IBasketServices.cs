e4d50ea9f37bb89f462fefc0d1b0eec0e05e2c65
gururajkl <gururajragavendra7@gmail.com>
2025-07-06 16:19:56 +0530

    Refactor and format code

--- src/WebApps/Shopping.Web/Services/IBasketServices.cs
+++ src/WebApps/Shopping.Web/Services/IBasketServices.cs
@@ -5,10 +5,10 @@ public interface IBasketServices
     // Annotations using the refit.
     [Get("/basket-service/basket/{userName}")]
     Task<GetBasketResponse> GetBasket(string userName);
-    
+
     [Post("/basket-service/basket")]
     Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
-    
+
     [Delete("/basket-service/basket/{userName}")]
     Task<DeleteBasketResponse> DeleteBasket(string userName);
 
--- src/WebApps/Shopping.Web/Services/ICatalogService.cs
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -5,10 +5,10 @@ public interface ICatalogService
     // Annotations using the refit.
     [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
     Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
-    
+
     [Get("/catalog-service/products/{id}")]
     Task<GetProductByIdResponse> GetProduct(Guid id);
-    
+
     [Get("/catalog-service/products/{category}")]
     Task<GetProductByCategoryResponse> GetProductByCategory(string category);
 }
