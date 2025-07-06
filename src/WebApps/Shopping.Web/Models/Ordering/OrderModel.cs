a09e54af578562d6da95c0eb89da5a3e7d106e6e
gururajkl <gururajragavendra7@gmail.com>
2025-07-06 14:31:38 +0530

    Create Order domain model in web application

---
+++ src/WebApps/Shopping.Web/Models/Ordering/OrderModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Shopping.Web.Models.Ordering;
+
+public record OrderModel(Guid Id, Guid CustomerId, string OrderName,
+    AddressModel ShippingAddress, AddressModel BillingAddress,
+    PaymentModel Payment, OrderStatus OrderStatus, List<OrderItemModel> OrderItems);
+
+public record AddressModel(string FirstName, string LastName, string EmailAddress, string AddressLine, string Country,
+    string State, string ZipCode);
+
+public record PaymentModel(string CardName, string CardNumber, string Expiration, string Cvv, int PaymentMethod);
+
+public record OrderItemModel(Guid OrderId, Guid ProductId, int Quantity, decimal Price);
+
+public enum OrderStatus
+{
+    Draft = 0,
+    Pending = 1,
+    Completed = 2,
+    Cancelled = 3,
+}
+
+// Wrapper class for OrderModel to be used in the API response.
+public record GetOrdersResponse(PaginatedResult<OrderModel> Orders);
+public record GetOrdersByNameResponse(IEnumerable<OrderModel> Orders);
+public record GetOrdersByCustomerResponse(IEnumerable<OrderModel> Orders);
