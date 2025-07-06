34e2f8e0e3454345ac6d07c2db4e7276cc34e3ac
gururajkl <gururajragavendra7@gmail.com>
2025-07-06 09:53:11 +0530

    Add YARP middleware and package reference
    Updated Program.cs to integrate YARP reverse proxy middleware, including service setup and request mapping. Modified YarpApiGateway.csproj to add Yarp.ReverseProxy package reference (v2.3.0) for reverse proxy functionality.

--- src/Services/ApiGateways/YarpApiGateway/Program.cs
+++ src/Services/ApiGateways/YarpApiGateway/Program.cs
@@ -2,8 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Setup the YARP middleware.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipelines.
+app.MapReverseProxy();
 
 app.Run();
